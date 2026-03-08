0037172305815ee0c808e4b93210c080ca429595
Sahil Malakar <sahilmalakar150@gmail.com>
2026-03-09 00:28:11 +0530

    feat: add prisma setup and database configuration for services

--- Booking/src/db/prisma.config.ts
+++ Booking/src/db/prisma.config.ts
@@ -3,15 +3,10 @@ import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 
-/**
- * Fix __dirname for ESM
- */
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-/**
- * Load root .env file
- */
 dotenv.config({
   path: path.resolve(__dirname, "../../.env"),
 });
